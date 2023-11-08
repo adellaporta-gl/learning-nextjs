7e0a723ee22aa5567aab0f840852abd6c8d60e65
adellaporta <adellaporta@hexacta.com>
2023-11-08 11:48:33 -0300

    Adding my solution

--- course_files/03/03-02_getStaticProps/.next/static/chunks/webpack.js
+++ course_files/03/03-02_getStaticProps/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08909e948013fe5c"; }
+/******/ 		__webpack_require__.h = function() { return "d049f331be89751a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- course_files/03/03-02_getStaticProps/pages/news/index.js
+++ course_files/03/03-02_getStaticProps/pages/news/index.js
@@ -1,16 +1,23 @@
 import Layout from "../../components/Layout"
-export default function News() {
-    return(
+export default function News({ results }) {
+  var index = 0;
+  return (
     <Layout>
       <h1>Top Stories</h1>
+      {results.map(result => <li key={++index}><a href={result.url} target="_blank">{result.title}</a></li>)}
     </Layout>)
 }
 
-const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
+const API_KEY = "QeZOCb5H3u4LEF76mCdFjtYzf2R1cjPK"
 export async function getStaticProps() {
   // The value of the `props` key will be
   //  passed to the `Home` component
+  const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
+  const rawData = await fetch(URL);
+  const jsonData = await rawData.json();
+  console.log("jsonData", jsonData);
+
   return {
-    props: {}
+    props: { results: jsonData.results }
   }
 }
